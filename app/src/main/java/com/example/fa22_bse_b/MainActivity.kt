f7d2d812400d433582192e21f77b8cf304549b18
Ali Usman <ali.usman@tapcheck.com>
2025-02-26 14:25:11 +0500

    Login Page with activity connection with UI components

--- app/src/main/java/com/example/fa22_bse_b/MainActivity.kt
+++ app/src/main/java/com/example/fa22_bse_b/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.fa22_bse_b
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.e(tag, "onCreate: function called ", )
+        setContentView(R.layout.login_page)
+
+        val loginButton: Button = findViewById(R.id.login_btn)
+        val emailET: EditText = findViewById(R.id.email_et)
+        val passwordEt: EditText = findViewById(R.id.password_et)
+
+
+        loginButton.setOnClickListener({
+            Log.e(tag, "Login button clicked")
+            Log.e(tag, "Email = ${emailET.getText()}")
+            Log.e(tag, "Password = ${passwordEt.getText()}")
+        })
 
     }
 
