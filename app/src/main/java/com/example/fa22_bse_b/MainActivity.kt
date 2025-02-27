b13bb6b5844ffbd5dd04d62f809d70980f15daa0
Ali Usman <ali.usman@tapcheck.com>
2025-02-27 10:59:46 +0500

    Login UI and Model data sharing

--- app/src/main/java/com/example/fa22_bse_b/MainActivity.kt
+++ app/src/main/java/com/example/fa22_bse_b/MainActivity.kt
@@ -10,23 +10,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e(tag, "onCreate: function called ", )
-        setContentView(R.layout.login_page)
-
-        val loginButton: Button = findViewById(R.id.login_btn)
-        val emailET: EditText = findViewById(R.id.email_et)
-        val passwordEt: EditText = findViewById(R.id.password_et)
-
-
-        loginButton.setOnClickListener({
-            Log.e(tag, "Login button clicked")
-            Log.e(tag, "Email = ${emailET.getText()}")
-            Log.e(tag, "Password = ${passwordEt.getText()}")
-        })
-
+        setContentView(R.layout.activity_main)
     }
 
 }
