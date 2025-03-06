051e685f9d64d8ad3dcdb533bf6fa3408b1d773c
Ali Usman <ali.usman@tapcheck.com>
2025-03-06 09:51:05 +0500

    Call Back Ultra revision

---
+++ app/src/main/java/com/example/fa22_bse_b/basics/callBack/main/main2.kt
@@ -0,0 +1,66 @@
+package com.example.fa22_bse_b.basics.callBack.main
+
+import android.R
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val phoneNumber: String,
+    val marks: Int
+)
+
+fun main() {
+
+//    val name = ""
+//    verifyName(name = name, validCallBack = {
+//        println("Your name is Valid")
+//    }, invalidCallBack = {
+//        println("Your name is Invalid")
+//    })
+
+    val student = Student(name = "Awais", age = 20, phoneNumber = "1234567", marks = -20)
+    verifyStudent(student = student, validNameCallBack = { message ->
+        println(message)
+    }, invalidNameCallBack = { message ->
+        println(message)
+    }, passCallBack = { message ->
+        println(message)
+    }, failCallBack = { message ->
+        println(message)
+    }, invalidMarksCallBack = { message ->
+        println(message)
+    })
+
+}
+
+
+fun verifyName(name: String, validCallBack: () -> Unit, invalidCallBack: () -> Unit) {
+    if (name == "") {
+        invalidCallBack.invoke()
+    } else {
+        validCallBack.invoke()
+    }
+}
+
+fun verifyStudent(
+    student: Student,
+    validNameCallBack: (String) -> Unit,
+    invalidNameCallBack: (String) -> Unit,
+    passCallBack: (String) -> Unit,
+    failCallBack: (String) -> Unit,
+    invalidMarksCallBack: (String) -> Unit
+) {
+    if (student.name.isEmpty()) {
+        invalidNameCallBack.invoke("'${student.name}' is a Invalid name")
+    } else {
+        validNameCallBack.invoke("'${student.name}' is a valid name")
+    }
+    if (student.marks > 49) {
+        passCallBack.invoke("'${student.name}' has passed with '${student.marks}'")
+    } else if (student.marks < 0) {
+        invalidMarksCallBack.invoke("Marks entered as '${student.marks}' for student ${student.name} are not valid marks")
+    } else {
+        failCallBack.invoke("'${student.name}' has failed with '${student.marks}'")
+    }
+}
+
