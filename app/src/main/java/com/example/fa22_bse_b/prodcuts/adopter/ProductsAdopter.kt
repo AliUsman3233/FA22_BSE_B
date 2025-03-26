5b50f8adf5cdd37f7da4156ae012f7634ec03d0a
Ali Usman <ali.usman@tapcheck.com>
2025-03-26 08:50:42 +0500

    Recycler View (Adopter Layer) Lecture 2

---
+++ app/src/main/java/com/example/fa22_bse_b/prodcuts/adopter/ProductsAdopter.kt
@@ -0,0 +1,43 @@
+package com.example.fa22_bse_b.prodcuts.adopter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fa22_bse_b.databinding.ProductRowDesignBinding
+import com.example.fa22_bse_b.prodcuts.model.Product
+
+class ProductsAdopter : ListAdapter<Product, ProductsAdopter.ProductViewHolder>(diffCheker) {
+
+
+    inner class ProductViewHolder(var productRowBinding: ProductRowDesignBinding) :
+        RecyclerView.ViewHolder(productRowBinding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val productRowBinding: ProductRowDesignBinding = ProductRowDesignBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ProductViewHolder(productRowBinding)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, location: Int) {
+        val item = getItem(location)
+        holder.productRowBinding.price.setText(item.price.toString())
+    }
+
+
+}
+
+val diffCheker = object : DiffUtil.ItemCallback<Product>() {
+    override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+        return false
+    }
+
+    override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+        return false
+    }
+
+}
---
+++ app/src/main/java/com/example/fa22_bse_b/prodcuts/ui/ProductsActivity.kt
@@ -0,0 +1,93 @@
+package com.example.fa22_bse_b.prodcuts.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.fa22_bse_b.R
+import com.example.fa22_bse_b.databinding.ProductListScreenBinding
+import com.example.fa22_bse_b.prodcuts.adopter.ProductsAdopter
+import com.example.fa22_bse_b.prodcuts.model.Product
+
+class ProductsActivity : AppCompatActivity() {
+
+    var binding: ProductListScreenBinding? = null
+    var productListAdopter: ProductsAdopter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.product_list_screen)
+
+        val productsList: MutableList<Product> = arrayListOf(
+            Product(
+                imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 1",
+                company = "XYZ",
+                price = 200.0,
+                discount = 5.0,
+                quantity = 2
+            ),
+            Product(
+                imageUrl =
+                "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 2",
+                company = "ABC",
+                price = 300.0,
+                discount = 6.0,
+                quantity = 20
+            ),
+            Product(
+                imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 3",
+                company = "JKBCSUII",
+                price = 900.0,
+                discount = 4.0,
+                quantity = 10
+            ),
+            Product(
+                imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 4",
+                company = "NJSNOIS",
+                price = 10000.0,
+                discount = 2.0,
+                quantity = 8
+            ),
+            Product(
+                imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 5",
+                company = "QWERTY",
+                price = 10.0,
+                discount = 0.01,
+                quantity = 5
+            ),
+            Product(
+                imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 6",
+                company = "XYZ",
+                price = 800.0,
+                discount = 1.0,
+                quantity = 3
+            ),
+            Product(
+                imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 7",
+                company = "TYUIO",
+                price = 423.0,
+                discount = 9.5,
+                quantity = 5
+            ),
+            Product(
+                imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fclock&psig=AOvVaw2izIh24c6zyivRrXyJcpta&ust=1743045315136000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiS_4HkpowDFQAAAAAdAAAAABAE",
+                desc = "Clock 8",
+                company = "XYZ",
+                price = 700.0,
+                discount = 6.0,
+                quantity = 7
+            )
+        )
+
+
+        productListAdopter = ProductsAdopter()
+        productListAdopter?.submitList(productsList)
+        Log.e(this@ProductsActivity.javaClass.simpleName, "onCreate: List = $productsList", )
+    }
+}
